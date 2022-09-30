5a859b329b00dc13b8cccd8893be7b425cd85bb1
svanderp-W <stijnvdpouw@gmail.com>
2022-09-30 17:55:47 +0200

    index searchbar added

---
+++ client/components/Base/SelectActivityCategory.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-select placeholder="Alle categorieen" icon="form-select">
+    <optgroup label="Black Sails">
+      <option value="flint">Flint</option>
+      <option value="silver">Silver</option>
+      <option value="vane">Vane</option>
+      <option value="billy">Billy</option>
+      <option value="jack">Jack</option>
+    </optgroup>
+
+    <optgroup label="Breaking Bad">
+      <option value="heisenberg">Heisenberg</option>
+      <option value="jesse">Jesse</option>
+      <option value="saul">Saul</option>
+      <option value="mike">Mike</option>
+    </optgroup>
+
+    <optgroup label="Game of Thrones">
+      <option value="tyrion-lannister">Tyrion Lannister</option>
+      <option value="jamie-lannister">Jamie Lannister</option>
+      <option value="daenerys-targaryen">Daenerys Targaryen</option>
+      <option value="jon-snow">Jon Snow</option>
+    </optgroup>
+  </b-select>
+</template>
+
+<script lang="ts"></script>
---
+++ client/components/QuickSearch.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-field>
+      <SelectActivityCategory />
+      <SelectLocation />
+      <b-button label="Zoek" type="is-primary" />
+    </b-field>
+  </div>
+</template>
+
+<script lang="ts">
+import SelectActivityCategory from '@/components/Base/SelectActivityCategory.vue';
+import SelectLocation from '@/components/Base/SelectLocation.vue';
+
+// TODO: get compontent data
+
+export default {
+  name: 'QuickSearch',
+  components: { SelectActivityCategory, SelectLocation },
+};
+</script>
