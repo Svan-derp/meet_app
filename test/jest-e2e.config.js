324e66ccb00bc56e656063c02c463cb145e9b78b
svanderp-W <stijnvdpouw@gmail.com>
2022-10-01 18:04:59 +0200

    eslint config

--- test/jest-e2e.config.js
+++ test/jest-e2e.config.js
@@ -1,5 +1,5 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
-  // eslint-disable-next-line
   ... require('./jest.config'),
   testMatch: ['**/?(*.)+(e2e-spec|e2e-test).(js|ts|tsx)'],
 };
