4ce5abd7a61ebc31cd9b2b88e43e97ae030b167e
svanderp-W <stijnvdpouw@gmail.com>
2022-10-04 18:29:57 +0200

    ???

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,11 +1,7 @@
 <template lang="vue">
 <div>
     <NavBar />
-<<<<<<< HEAD
     <nuxt />
-=======
-    <Nuxt />
->>>>>>> renamed-branch
     <!-- TODO: make sure all notifications are called from here -->
   </div>
 </template>
