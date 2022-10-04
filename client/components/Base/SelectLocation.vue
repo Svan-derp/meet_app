ec3baac51ef783372ab12ba5e839b4a7e16894e5
svanderp-W <stijnvdpouw@gmail.com>
2022-10-04 17:25:02 +0200

    index searchbar added

---
+++ client/components/Base/SelectLocation.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-autocomplete
+    :data="['jQuery', 'Vue', 'React']"
+    placeholder="Locatie"
+    icon="map-marker"
+    expanded
+    clearable
+    @select="(option) => (selected = option)"
+  >
+    <template #empty>No results found</template>
+  </b-autocomplete>
+</template>
+<script lang="ts"></script>
